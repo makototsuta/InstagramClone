3532b6f33f38a19f4158c1d1e673a2db22061b78
makoto tsuta <makoto1850@gmail.com>
2020-01-23 18:11:21 +0900

    ログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'sessions/new'
   resources :pictures
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
 end
