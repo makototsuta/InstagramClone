17fe7d5536725eb0edfe90dcc4b46f005a386df9
makoto tsuta <makoto1850@gmail.com>
2020-01-23 18:00:48 +0900

    セッションコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :pictures
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create, :show]
 end
