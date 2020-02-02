36066381b83d35009eaf6281a0cedcaaba422372
makoto tsuta <makoto1850@gmail.com>
2020-02-03 08:43:57 +0900

    production.rb誤記修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'peaceful-bayou-12976.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'infinite-mountain-83385.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
